bcc3771f37627172ddb183cb07776a4a105fda32
SodaCris <18463922396@163.com>
2021-11-08 12:15:15 +0800

    remove insecure key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r^1r(jxc@h0ewt&#dirh7hqhvhzk7h%9qrepwl1%v$!fmz3ub^'
+import local_settings
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
